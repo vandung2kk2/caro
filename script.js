209f5359f6be0d984eb34c56f07dd10c34b3b6cd
Luu Van Dung <42308652+vandung2kk2@users.noreply.github.com>
2021-02-03 21:35:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ window.onload = function(){
 				cells[i].classList.add("last-move");
 				lastMove = i;
 				boardMatx[position[0]][position[1]] = turn%2+1;
-				console.log(check([position[0],position[1]]));
+				if(check([position[0],position[1]])) alert("win r";
 				turn++;
 			}
 		})
